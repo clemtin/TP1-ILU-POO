a28f0d2f7a23bb8d5a9f61b6d5009c9dbe79e3df
clemtin <boillatclement@gmail.com>
2024-03-01 17:37:37 +0100

    fin tp2 enfin j'en ai mis du temps

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,6 +3,8 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private int nbEquipement = 0;
+	private Equipement equipements[] = new Equipement[2];
 
 	public Romain(String nom, int force) {
 		assert force >= 0;
@@ -33,7 +35,43 @@ public class Romain {
 		}
 	}
 
+	public void donner(Equipement equipement, int a) {
+		equipements[a] = equipement;
+		nbEquipement++;
+		System.out.println("Le soldat" + nom + "s'equipe d'un" + equipement + "!");
+
+	}
+
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 2:
+			System.out.println("Le soldat" + nom + " est desja bien protégé");
+			break;
+
+		case 1:
+			if (equipements[0] == equipement) {
+				System.out.println(" Le soldat" + nom + " possède déjà un " + equipement + " !");
+			} else {
+				donner(equipement, 1);
+			}
+			break;
+
+		case 0:
+			donner(equipement, 0);
+			break;
+		}
+	}
+
 	public static void main(String[] args) {
-		Romain minus = new Romain("minus", 6);
+		Romain minus = new Romain("Minus", 6);
+		System.out.println(minus.getNom());
+		System.out.println(minus);
+		minus.prendreParole();
+		minus.parler("Yo");
+		minus.recevoirCoup(5);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+
 	}
 }
